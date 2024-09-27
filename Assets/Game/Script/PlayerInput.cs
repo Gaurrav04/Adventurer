57a8347d0e4fcb1cee94ca4b5445a74946fc5950
Gaurrav04 <94034679gd@gmail.com>
2024-09-27 11:57:00 +0530

    Commit gaurav

---
+++ Assets/Game/Script/PlayerInput.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float HorizontalInput;
+
+    public float VericalInput;
+
+    public bool MouseButtonDown;
+
+    public bool SpaceKeyDown;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if( !MouseButtonDown && Time.timeScale != 0)
+        {
+            MouseButtonDown = Input.GetMouseButtonDown(0);
+        }
+
+        if (!SpaceKeyDown && Time.timeScale != 0)
+        {
+            SpaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+        }
+
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        VericalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnDisable()
+    {
+        ClearCache();
+    }
+
+    public void ClearCache()
+    {
+        MouseButtonDown = false;
+        SpaceKeyDown = false;
+        HorizontalInput = 0;
+        VericalInput = 0;
+    }
+}
